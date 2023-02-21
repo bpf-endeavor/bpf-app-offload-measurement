2c6f10d9617fae960ef330a67a8b42afa18a5966
farbod <fshahinfar1@gmail.com>
2023-02-21 17:36:24 +0000

    add benchmark, varying number of instructions

--- src/userspace/server/main.c
+++ src/userspace/server/main.c
@@ -1,11 +1,26 @@
 #define _GNU_SOURCE
 
+static int instructions;
+
 /* If a value should be shared across multiple message of a socket place it in
  * this struct */
 struct client_ctx { };
 
 #include "userspace/log.h"
 #include "userspace/sock_app.h"
+
+#define RECV(fd, buf, size, flag)  {                  \
+	ret = recv(fd, buf, size, flag);              \
+	if (ret == 0)                                 \
+		return 1;                             \
+	if (ret < 0) {                                \
+		if (errno != EWOULDBLOCK) {           \
+			/* all message was received and connection is closed */ \
+			return 1;                     \
+		}                                     \
+		return 0;                             \
+	}                                             \
+}
 
 
 /* Handle a socket message
@@ -15,10 +30,16 @@ struct client_ctx { };
  * */
 int handle_client(int client_fd, struct client_ctx *ctx)
 {
-	int ret;
+	int ret, value;
 	char buf[BUFSIZE];
-	ret = recv(client_fd, buf, BUFSIZE, 0);
-	INFO("%s\n", buf);
+
+	/* Receive message and check the return value */
+	RECV(client_fd, buf, BUFSIZE, 0);
+	value = *((int *)buf); /* read 4 bytes of message */
+	for (int i = 0; i < instructions; i++) {
+		value = value * 2;
+	}
+	*((int *)buf) = value;
 	return 1;
 }
 
@@ -28,8 +49,8 @@ int main(int argc, char *argv[])
 	struct socket_app app;
 
 	/* parse args */
-	if (argc < 3) {
-		INFO("usage: prog <core> <ip>\n");
+	if (argc < 4) {
+		INFO("usage: prog <core> <ip> <num insts>\n");
 		return 1;
 	}
 	app.core = atoi(argv[1]);
@@ -37,6 +58,10 @@ int main(int argc, char *argv[])
 	app.ip = argv[2];
 	app.count_workers = 1;
 	app.sock_handler = handle_client;
+
+	/* How many instructions to spend per packet */
+	instructions = atoi(argv[3]);
+
 	ret = run_server(&app);
 	if (ret != 0) {
 		ERROR("Failed to run server!\n");
