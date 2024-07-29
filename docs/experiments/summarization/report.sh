1fff0643109bd3cca9a6a6daee43e82d72d17a73
Farbod Shahinfar <fshahinfar1@gmail.com>
2024-07-29 16:46:47 +0330

    add summary effect exp results

---
+++ docs/experiments/summarization/report.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+FILES=( )
+VAL=( )
+ERR=( )
+for f in *p.txt; do
+	echo $f
+	FILES+=( $f )
+	T=$(cat $f  | awk '{print $2}' | ../../latency_script.py  | grep +-)
+	m=$(echo $T | awk '{print $4}')
+	p=$(echo $T | awk '{print $6}')
+	VAL+=( $m )
+	ERR+=( $p )
+done
+
+echo ${FILES[@]}
+echo ${VAL[@]}
+echo ${ERR[@]}
