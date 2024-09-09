70588af821156da723d3b1da770c69c1e93e9e6f
Farbod Shahinfar <fshahinfar1@gmail.com>
2024-09-09 10:57:04 +0000

    update

--- scripts/memcpy_bench/script.sh
+++ scripts/memcpy_bench/script.sh
@@ -5,9 +5,9 @@ cd $DIR
 
 r=100
 for i in $(seq $r); do
-	sudo ./build/runner -b ./build/memcpy.o | grep "benchmark res" | awk '{print $3 / 100}' &>> $CURDIR/ebpf_samples.txt
+	# sudo ./build/runner -b ./build/memcpy.o | grep "benchmark res" | awk '{print $3 / 100}' &>> $CURDIR/ebpf_samples.txt
+	# sleep 1
+	# ./build/memcpy_bench | awk '{print substr($3,2, length($3)-1)}' &>> $CURDIR/user_simd_samples.txt
 	sleep 1
-	./build/memcpy_bench | awk '{print substr($3,2, length($3)-2)}' &>> $CURDIR/user_simd_samples.txt
-	sleep 1
-	./build/memcpy_bench_no_simd | awk '{print substr($3,2, length($3)-2)}' &>> $CURDIR/user_no_simd_samples.txt
+	./build/memcpy_bench_no_simd | awk '{print substr($3,2, length($3)-1)}' &>> $CURDIR/user_no_simd_samples.txt
 done
