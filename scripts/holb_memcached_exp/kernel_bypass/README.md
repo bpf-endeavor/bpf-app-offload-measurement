02e97b6554a008448959ed7f3b20ed3e9e7cba76
Farbod Shahinfar <fshahinfar1@gmail.com>
2025-04-21 11:29:03 +0000

    add some notes about seastar and memcached + af_xdp experiments

---
+++ scripts/holb_memcached_exp/kernel_bypass/README.md
@@ -0,0 +1,35 @@
+# Experiments
+
+1. Comparing the Memcached AF\_XDP vs. Socket API
+2. Comparing the BMC benefit for AF\_XDP vs Socket API
+3. Showing the BMC slowdown for the background traffic
+
+## Experiment Setup
+
+**Memcached AF_XDP:**
+
+I have changed Memcached implementation insdie the Seastar repo to (a) connect
+to AF\_XDP and (b) work with BMC. Look under the `patches/memcached_seastar` for
+changes.
+
+**Enable Seastar Memcached + AF_XDP sockets:**
+
+1. Apply the patch `0001-Attach-seastar-memcached-to-AF_XDP.patch`
+	- The patch has hardcoded configuration of AF\_XDP queue (update it after applying the patch)
+2. `./configure.py --mode=release --enable-dpdk`
+3. `ninja -C ./build/release`
+4. Use script `run_seastar_memcd.sh` to run experiment
+	- The script has hardcoded configuration
+
+**Enable Seastar Memcached + AF_XDP sockets + BMC:**
+
+1. Use the my-seastar repository that I forked
+
+or 
+
+1. Apply the patches under `patches/memcached_seastar/with_bmc/`
+2. Compile as described above (for Memcached + AF\_XDP)
+3. Clone bmc and apply the patches required to send packets to AF\_XDP socket
+	- Patches are at `patches/bmc/send_to_af_xdp/`
+
+
--- scripts/holb_memcached_exp/kernel_bypass/run_seastar_memcd.sh
+++ scripts/holb_memcached_exp/kernel_bypass/run_seastar_memcd.sh
@@ -1,13 +1,61 @@
 #! /bin/bash
 
+# set -x
+
 # Assumptions:
 # System has hugepages and it is mounted in /dev/hugepages (look at TAS readme)
 
-Memcd="/home/farbod/seastar/build/release/apps/memcached/memcached"
+mode="plain"
+# mode="bmc"
+
+if [ -z $NET_IFACE ]; then
+	echo "NET_IFACE is not set"
+	exit 1
+fi
+
+launch_bmc() {
+	# launch bmc on the given interface
+	# ifindex=$(ip -j address show dev $NET_IFACE | jq '.[0].ifindex')
+	# nohup sudo $BMC_BIN $ifindex &
+	echo "Launching AF_XDP Memcached + BMC ..."
+	echo "The seastar library has been modified to bind to AF_XDP driver."
+	echo "Unfortunately the configurations are hard-coded (look at the patch)"
+	echo "After running the program, it will load the XDP program."
+	echo "You need to route the traffic towards the correct queue using flow-steering"
+
+	echo "For example:"
+	printf "\tsudo ethtool -U $NET_IFACE flow-type udp4 dst-port 11211 action 26\n\tsudo ethtool -U $NET_IFACE flow-type tcp4 dst-port 11211 action 26\n\n"
+}
+
+build_type=release
+# build_type=debug
+
+main() {
+case $mode in
+	plain)
+		Memcd="/home/farbod/seastar/build/$build_type/apps/memcached/memcached"
+		;;
+	bmc)
+		Memcd="/home/farbod/my-seastar/build/$build_type/apps/memcached/memcached"
+		launch_bmc
+		;;
+esac
+
+
+# sudo $Memcd --help-seastar
+# exit 0
 
 sudo $Memcd -c 1 --cpuset 11 \
-	-m 4G \
+	-m 8G \
 	--poll-mode --dpdk-pmd \
 	--network-stack native \
 	--host-ipv4-addr 192.168.200.101 --netmask-ipv4-addr 255.255.255.0 \
 	--collectd 0
+
+# Make sure the XDP program is deatched
+sudo ip link set dev $NET_IFACE xdp off
+
+echo Done!
+}
+
+main
