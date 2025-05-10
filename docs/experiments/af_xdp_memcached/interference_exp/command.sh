5c09197166d57ee580bca6cb0206a9fc68a40a05
Farbod Shahinfar <fshahinfar1@gmail.com>
2025-05-10 14:32:53 +0200

    udpate results and figures

--- docs/experiments/af_xdp_memcached/interference_exp/command.sh
+++ docs/experiments/af_xdp_memcached/interference_exp/command.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
-cat ./xsk_cache/baseline/m2_lat_samples_*.txt | awk '{print $3}' | sort -n > m2_lat_baseline.txt
-cat ./xsk_cache/with_bmc/m2_lat_samples_*.txt | awk '{print $3}' | sort -n > m2_lat_bmc.txt
 
+get_latency_values() {
+	cat $1 | awk '/200 / {print $3}' | sort -n > $2
+}
+
+scale() {
+	_T=/tmp/__t_scale
+	for i in $(seq $2); do
+		cat $1 >> $_T
+	done
+	cat $_T
+	rm $_T
+}
+
+get_latency_values "./xsk_cache/baseline/m2_lat_samples_*.txt" m2_lat_baseline.txt
+get_latency_values "./xsk_cache/with_bmc/m2_lat_samples_*.txt" m2_lat_bmc.txt
+
+# get_latency_values "./xsk_cache/baseline/m1_lat_samples_*.txt" /tmp/m1_lat_baseline.txt
+# get_latency_values "./xsk_cache/with_bmc/m1_lat_samples_*.txt" /tmp/m1_lat_bmc.txt
+# scale /tmp/m1_lat_baseline.txt 500 > all_lat_baseline.txt
+# scale /tmp/m1_lat_bmc.txt 500 > all_lat_bmc.txt
+
+get_latency_values "./xsk_cache/baseline/m*_lat_samples_*.txt" all_lat_baseline.txt
+get_latency_values "./xsk_cache/with_bmc/m*_lat_samples_*.txt" all_lat_bmc.txt
